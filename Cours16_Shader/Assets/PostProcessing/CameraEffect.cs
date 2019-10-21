155422424aa4e588709e16b85fe4ff873481dbdf
Zachary Alexandre <zachary.alexandre430@gmail.com>
2019-10-21 17:03:02 -0400

    Red

---
+++ Cours16_Shader/Assets/PostProcessing/CameraEffect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraEffect : MonoBehaviour {
+
+    private Material mat;
+
+    void Awake()
+    {
+        mat = new Material(Shader.Find("Custom/post"));
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, mat);
+    }
+}
